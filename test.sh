f58a9f46874da6c43302686076c295c765572c40
AM00zero <1585801268@qq.com>
2020-05-06 19:17:27 +0800

    初始化作业仓库

--- test.sh
+++ test.sh
@@ -3,6 +3,9 @@
 # Author: am
 # Created Time: Sun May  3 11:13:46 2020
 #########################################################################
-#!/usr/bin/env/bash
+unused_vars="This is a unused variable example"
 
-awk -F "\t" 'BEGIN{d20=0;bt2030=0;up30=0}NR>1{print $6}' chap0x04/worldcupplayerinfo.tsv
+echo "在当前目录下建立每次作业的子目录"
+echo "根据需要在 .travis.yml 中添加每次作业的脚本执行方法"
+echo "代码在执行过程中产生的错误信息统一写入到 error.log"
+
